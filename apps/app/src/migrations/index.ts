57a574b827ee6114db8ecd88fe33ac330f0f999d
unknown <top506@yandex.ru>
2025-10-29 17:14:22 +0300

    updated migrations

--- apps/app/src/migrations/index.ts
+++ apps/app/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251024_171847 from './20251024_171847';
 import * as migration_20251028_142433 from './20251028_142433';
+import * as migration_20251029_135641 from './20251029_135641';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251028_142433.up,
     down: migration_20251028_142433.down,
-    name: '20251028_142433'
+    name: '20251028_142433',
+  },
+  {
+    up: migration_20251029_135641.up,
+    down: migration_20251029_135641.down,
+    name: '20251029_135641'
   },
 ];
