aaa1938a3183dfcdb5049cc6ea052744267dfc18
Paul Ledovskikh <pl@altrp.org>
2025-11-03 17:03:40 +0300

    feat: update humans schema to use string default for order, and enhance schema exports with taxonomy and employees

---
+++ apps/site/functions/_shared/schema.ts
@@ -0,0 +1,27 @@
+export * from './schema/index';
+
+export type {
+	BaseMove,
+	Product,
+	ProductVariant,
+	Deal,
+	DealProduct,
+	Contractor,
+	Base,
+	Wallet,
+	WalletTransaction,
+	Location,
+	NewBaseMove,
+	NewProduct,
+	NewProductVariant,
+	NewDeal,
+	NewDealProduct,
+	NewContractor,
+	NewBase,
+	NewWallet,
+	NewWalletTransaction,
+	NewLocation,
+} from './schema/types';
+
+import * as schemaExports from './schema/index';
+export const schema = schemaExports;
